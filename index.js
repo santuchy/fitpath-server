08a99d618c12f66caccb0002b7cd36073754dddc
Santuchy <santuchowdhuryofficial@gmail.com>
2025-07-29 18:57:24 +0600

    everything checked and worked properly

--- index.js
+++ index.js
@@ -19,9 +19,8 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-var serviceAccount = require("./firebase-admin-key.json");
+const decodedKey = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(decodedKey);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -51,7 +50,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db("fitPathDB");
         const usersCollection = db.collection("users");
@@ -845,9 +844,9 @@ async function run() {
             res.send({ message: `Welcome, ${req.user.email}` });
         });
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
